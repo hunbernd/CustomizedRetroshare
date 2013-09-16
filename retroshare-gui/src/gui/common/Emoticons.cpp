0c431f4305299e4f8e25154722dc3dc4865643fa
hunbernd <hunbernd@outlook.com>
2013-09-16 16:02:02 +0200

    Emoticons by Morph (cherry picked from commit 104ee33166dee424909eb69946a484549c3864ed)

--- retroshare-gui/src/gui/common/Emoticons.cpp
+++ retroshare-gui/src/gui/common/Emoticons.cpp
@@ -40,29 +40,28 @@ void Emoticons::load()
     QString sm_codes;
     bool internalEmoticons = true;
 
-#if defined(Q_OS_WIN32)
+//#if defined(Q_OS_WIN32)
     // first try external emoticons
     QFile sm_file(QApplication::applicationDirPath() + "/emoticons/emotes.acs");
-    if(sm_file.open(QIODevice::ReadOnly))
-    {
+    if(sm_file.open(QIODevice::ReadOnly)) {
         internalEmoticons = false;
     } else {
-        // then embedded emotions
+        // then embedded emoticons
         sm_file.setFileName(":/smileys/emotes.acs");
-        if(!sm_file.open(QIODevice::ReadOnly))
-        {
+        if(!sm_file.open(QIODevice::ReadOnly)) {
             std::cout << "error opening ressource file" << std::endl ;
             return ;
         }
     }
-#else
-    QFile sm_file(QString(":/smileys/emotes.acs"));
-    if(!sm_file.open(QIODevice::ReadOnly))
-    {
-        std::cout << "error opening ressource file" << std::endl ;
-        return ;
-    }
-#endif
+//#else
+    // if not Windows, then embedded emoticons only
+//    QFile sm_file(QString(":/smileys/emotes.acs"));
+//    if(!sm_file.open(QIODevice::ReadOnly))
+//    {
+//        std::cout << "error opening ressource file" << std::endl ;
+//        return ;
+//    }
+//#endif
 
     sm_codes = sm_file.readAll();
     sm_file.close();
@@ -73,34 +72,28 @@ void Emoticons::load()
     int i = 0;
     QString smcode;
     QString smfile;
-    while(sm_codes[i] != '{')
-    {
+    while(sm_codes[i] != '{') {
         i++;
     }
-    while (i < sm_codes.length()-2)
-    {
+    while (i < sm_codes.length()-2) {
         smcode = "";
         smfile = "";
 
-        while(sm_codes[i] != '\"')
-        {
+        while(sm_codes[i] != '\"') {
             i++;
         }
         i++;
-        while (sm_codes[i] != '\"')
-        {
+        while (sm_codes[i] != '\"') {
             smcode += sm_codes[i];
             i++;
         }
         i++;
 
-        while(sm_codes[i] != '\"')
-        {
+        while(sm_codes[i] != '\"') {
             i++;
         }
         i++;
-        while(sm_codes[i] != '\"' && sm_codes[i+1] != ';')
-        {
+        while(sm_codes[i] != '\"' && sm_codes[i+1] != ';') {
             smfile += sm_codes[i];
             i++;
         }
@@ -128,9 +121,10 @@ void Emoticons::showSmileyWidget(QWidget *parent, QWidget *button, const char *s
 
     const int buttonWidth = 26;
     const int buttonHeight = 26;
-    const int countPerLine = 9;
+    //const int countPerLine = 9;
+    const int countPerLine = 15;
 
-    int rowCount = (Smileys.size()/countPerLine) + ((Smileys.size() % countPerLine) ? 1 : 0);
+    int rowCount = (Smileys.size() / countPerLine) + ((Smileys.size() % countPerLine) ? 1 : 0);
 
     smWidget->setAttribute( Qt::WA_DeleteOnClose);
     smWidget->setWindowTitle("Emoticons");
@@ -141,34 +135,41 @@ void Emoticons::showSmileyWidget(QWidget *parent, QWidget *button, const char *s
     //         It was EmoticonSelector::alignTo(QWidget* w) function there
     //         comments are Polish, I dont' know how does it work...
     // oblicz pozycj� widgetu do kt�rego r�wnamy
+    // calculate the position of the widget to ??????????????????
     // oblicz rozmiar selektora
+    // calculate the size selector
     QPoint pos = button->mapToGlobal(QPoint(0,0));
     QSize e_size = smWidget->sizeHint();
     // oblicz rozmiar pulpitu
     QSize s_size = QApplication::desktop()->size();
     // oblicz dystanse od widgetu do lewego brzegu i do prawego
+    //calculate distances from the widget to the left bank and the right
     int l_dist = pos.x();
     int r_dist = s_size.width() - (pos.x() + button->width());
     // oblicz pozycj� w zale�no�ci od tego czy po lewej stronie
     // jest wi�cej miejsca czy po prawej
     int x;
-    if (l_dist >= r_dist)
+    if (l_dist >= r_dist) {
         x = pos.x() - e_size.width();
-    else
+    } else {
         x = pos.x() + button->width();
+    }
+
     // oblicz pozycj� y - centrujemy w pionie
     int y = pos.y() + button->height()/2 - e_size.height()/2;
-    // je�li wychodzi poza doln� kraw�d� to r�wnamy do niej
-    if (y + e_size.height() > s_size.height())
-        y = s_size.height() - e_size.height();
 
+    // je�li wychodzi poza doln� kraw�d� to r�wnamy do niej
+    if (y + e_size.height() > s_size.height()) {
+        y = s_size.height() - e_size.height();
+    }
     if (above) {
         y -= rowCount*buttonHeight;
     }
 
     // je�li wychodzi poza g�rn� kraw�d� to r�wnamy do niej
-    if (y < 0)
+    if (y < 0) {
         y = 0;
+    }
     // ustawiamy selektor na wyliczonej pozycji
     smWidget->move(x, y);
 
@@ -176,8 +177,7 @@ void Emoticons::showSmileyWidget(QWidget *parent, QWidget *button, const char *s
     y = 0;
 
     QHashIterator<QString, QString> i(Smileys);
-    while(i.hasNext())
-    {
+    while(i.hasNext()) {
         i.next();
         QPushButton *smButton = new QPushButton("", smWidget);
         smButton->setGeometry(x*buttonWidth, y*buttonHeight, buttonWidth, buttonHeight);
@@ -186,12 +186,14 @@ void Emoticons::showSmileyWidget(QWidget *parent, QWidget *button, const char *s
         smButton->setToolTip(i.key());
         smButton->setStyleSheet("QPushButton:hover {border: 3px solid white; border-radius: 2px;}");
         smButton->setFlat(true);
+
         ++x;
-        if(x >= countPerLine)
-        {
+
+        if(x >= countPerLine) {
             x = 0;
             y++;
         }
+
         QObject::connect(smButton, SIGNAL(clicked()), parent, slotAddMethod);
         QObject::connect(smButton, SIGNAL(clicked()), smWidget, SLOT(close()));
     }
